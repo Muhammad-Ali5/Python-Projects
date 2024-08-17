97d7ac844adee8cbd6fed353da48e826b9e1240d
Muhammad-Ali5 <maliuetm507@gmail.com>
2024-08-17 13:40:42 +0500

    Add files via upload

---
+++ Automatic message sender/sendmassage.py
@@ -0,0 +1,11 @@
+import pywhatkit as pyw
+
+pyw.sendwhatmsg('+923105540387','kher sha pera sanga ee',14,52)
+# first of all you will make file on the desktop than 
+''' Step 1. you open the cmd 
+    step 2. In cmd you write the cd Desktop and press enter.
+    step 3. than we enter our folder namem like (cd mini-project) , in which our python program is present.and press enter.
+    step 4. Than we write python and our file name and press enter.
+    After this our program is start according to your time , which you given in the program.
+'''
+
