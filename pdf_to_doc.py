ac56ce60fa1a7ddb25c5d623908bd1ee9d002337
Muhammad-Ali5 <maliuetm507@gmail.com>
2024-08-23 16:48:44 +0500

    Add files via upload

---
+++ pdf_to_doc.py
@@ -0,0 +1,16 @@
+from docx2pdf import convert
+convert("E:\python Programs\projects.py\pdf_to_word_viceVersa\jani.docx", "new_pdf.pdf")
+
+
+from pdf2docx import Converter
+# old_file = "E:\python Programs\projects.py\pdf_to_word_viceVersa\Art book.pdf"
+# new_doc = "jani.docx"
+
+# # Create a PDF to DOCX converter object
+# obj = Converter(old_file)
+
+# # Perform the conversion
+# obj.convert(new_doc)
+
+# # Close the converter
+# obj.close()
